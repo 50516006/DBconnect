2658d8122a990e22f0f9b1c711ea6234a3121d7d
50516006 <windrgon8@gmail.com>
2017-01-23 18:19:06 +0900

    ioioioi

---
+++ src/dbconnect/DBconnect_main.java
@@ -0,0 +1,18 @@
+package dbconnect;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class DBconnect_main {
+
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Graph_view graph= new Graph_view();
+		graph.setBounds(5, 5, 800,600);
+		graph.setVisible(true);
+
+
+}}
